a8a5fde8467e706bef0a8d4c6782d3f971142276
jwisniewski30 <89911245+jwisniewski30@users.noreply.github.com>
2022-06-19 23:24:46 -0400

    Add files via upload
    Exercise 2.1

---
+++ JWisniewski2.1.py
@@ -0,0 +1,44 @@
+"""
+Created on Sun Jun 19 21:40:01 2022
+Assignment 2.1
+@author: jwisniewski
+"""
+
+# Display the text
+print('Hello World! I wonder why this is always the default coding text to start with.')
+# Add two numbers together
+x = input("Type a number: ")
+y = input("Type another number: ")
+sum = int(x) + int(y)
+print("The sum is: ", sum)
+print("Now let's subtract")
+# Subtract a number from another number
+x = input("Type a number: ")
+y = input("Type another number: ")
+difference = int(x) - int(y)
+print("The difference is: ", difference)
+print("Now let's multiply")
+# Multiply two numbers
+x = input("Type a number: ")
+y = input("Type another number: ")
+product = int(x) * int(y)
+print("The product is: ", product)
+print("Now let's divide")
+# Divide two numbers
+x = input("Type a number: ")
+y = input("Type another number: ")
+quotient = int(x) / int(y)
+print("The quotient is: ", quotient)
+# Concatenate two strings together (any words)
+f = "Foot"
+b = "ball"
+print(f+b)
+# Create a list of 4 items (can be strings, numbers, both)
+Alabama_list = ['Huntsville', 'Birmingham', 'Lincoln', 'Piedmont', 'Montgomery']
+print(Alabama_list)
+# Append an item to your list (again, can be a string, number)
+Alabama_list.append("Tuscaloosa")
+print(Alabama_list)
+# Create a tuple with 4 items (can be strings, numbers, both)
+TennTuple = ('Nashville', 'Jackson', 'Memphis')
+print(TennTuple)
